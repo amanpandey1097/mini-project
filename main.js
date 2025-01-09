219b2019300df5730a5f26fc8afe6afa6417cb61
Aayush-192 <aayushpandey19225@gmail.com>
2025-01-09 23:10:21 +0530

    updated

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 alert("WELCOME!!!!!!")
+
+// Simple search button functionality
+const searchButton = document.querySelector('header .search-bar button');
+const searchInput = document.querySelector('header .search-bar input');
+
+searchButton.addEventListener('click', () => {
+    const query = searchInput.value;
+    if (query.trim() !== "") {
+        alert(`Searching for: ${query}`);
+    } else {
+        alert("Please enter a search term.");
+    }
+});
